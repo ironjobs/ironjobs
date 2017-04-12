510061689d317bc00c545f56edd1fb7394f2c864
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-04-12 17:01:09 -0400

    for each added

--- server/routes/jobs.routes.js
+++ server/routes/jobs.routes.js
@@ -1,17 +1,32 @@
 const jobRouter = require('express').Router();
 
-let allJobs = ['Job1', 'Job2', 'Job3'];
+let allJobs = [];
 
 jobRouter.get('/', function showAllJobs(req, res, next) {
-
+  allJobs.map(function output(allJobs){
+    return {
+      id: req.body.id,
+      company: req.body.company,
+      link: req.body.link
+    };
+  });
   res.json(allJobs);
+
 });
 
 function addAJob(req, res, next) {
   console.log('Incoming data for POST', req.body);
 
-  allJobs.push(req.body.job);
-  res.json({ message:' New job added!', theJobAdded: req.body});
+  let job = {
+    id:'364',
+    company:req.body.company,
+    link:req.body.link,
+    notes:req.body.notes,
+    createTime:Date.now()
+  };
+
+  allJobs.push(job);
+  res.json({ message:' New job added!', theJobAdded: job});
 
 }
 jobRouter.post('/', addAJob);
